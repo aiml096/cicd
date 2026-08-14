5d8e98a42f0b1b366be7f6066f958647d17c7f31
aiml096 <22cseaiml096.potnuruvamsikrishna@giet.edu>
2026-08-14 21:23:29 +0530

    ci/cd testing

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
     import uvicorn
