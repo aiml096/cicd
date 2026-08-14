9ee0e5bbb15ee15a3092e7cf979165c253ea6684
aiml096 <22cseaiml096.potnuruvamsikrishna@giet.edu>
2026-08-14 22:06:11 +0530

    testing-3

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ async def root():
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
